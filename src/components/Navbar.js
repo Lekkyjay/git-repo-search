032c373d400d857b85291d02a5307b20c2a65bad
Lekkyjay <lekkyjayjay@yahoo.com>
2021-05-10 21:42:34 +0200

    Nav dropdown functionality done.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FaCaretDown, FaCaretUp } from 'react-icons/fa'
 
 export default function Navbar() {
+  const [dropDown, setDropDown] = useState(false)
+
   return (
-    <div>
-      <h1>Namvbar</h1>
-    </div>
+    <nav>
+      <div className="nav-container container">
+        <h1>Repo Search App</h1>
+        <ul className="nav">
+          <li>
+            <NavLink exact to="/">Search</NavLink>
+          </li>
+          <li><NavLink to="/history">History</NavLink></li>
+        </ul>
+        <div className="more">
+          {/* <button>More</button> */}
+          { dropDown 
+              ? <FaCaretUp className="caret-icon" onClick={()=>setDropDown(!dropDown)} /> 
+              : <FaCaretDown className="caret-icon" onClick={()=>setDropDown(!dropDown)}/> 
+          }
+          
+          { dropDown && 
+            <div className="dropdown">          
+              <ul>
+                <li><NavLink to="/page1" onClick={()=>setDropDown(false)}>Page 1</NavLink></li>
+                <li><NavLink to="/page2" onClick={()=>setDropDown(false)}>Page 2</NavLink></li>
+              </ul>
+            </div>
+          }
+        </div>
+      </div>
+    </nav>
   )
 }
