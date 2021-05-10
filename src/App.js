13c2d4df48fc8d355ab7e72feaa65ed60d282aa6
Lekkyjay <lekkyjayjay@yahoo.com>
2021-05-10 20:45:58 +0200

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import History from './pages/History';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Search Repo App</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Search} />
+        <Route path='/history' component={History} />
+        <Route path='/page1' component={Page1} />
+        <Route path='/page2' component={Page2} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
