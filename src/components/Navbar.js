d223d896bbca5a986d6b3e164dbcd3bd14046da1
Lekkyjay <lekkyjayjay@yahoo.com>
2021-05-11 12:45:58 +0200

    Mobile nav functionality done.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,41 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { FaCaretDown, FaCaretUp } from 'react-icons/fa'
+import { FaBars, FaCaretDown, FaCaretUp, FaTimes } from 'react-icons/fa'
 
 export default function Navbar() {
   const [dropDown, setDropDown] = useState(false)
+  const [mobileNav, setMobileNav] = useState(false)
 
   return (
     <nav>
       <div className="nav-container container">
         <h1>Repo Search App</h1>
-        <ul className="nav">
-          <li>
-            <NavLink exact to="/">Search</NavLink>
-          </li>
-          <li><NavLink to="/history">History</NavLink></li>
-        </ul>
-        <div className="more">
-          {/* <button>More</button> */}
-          { dropDown 
-              ? <FaCaretUp className="caret-icon" onClick={()=>setDropDown(!dropDown)} /> 
-              : <FaCaretDown className="caret-icon" onClick={()=>setDropDown(!dropDown)}/> 
-          }
-          
-          { dropDown && 
-            <div className="dropdown">          
-              <ul>
-                <li><NavLink to="/page1" onClick={()=>setDropDown(false)}>Page 1</NavLink></li>
-                <li><NavLink to="/page2" onClick={()=>setDropDown(false)}>Page 2</NavLink></li>
-              </ul>
-            </div>
-          }
+        <div className={ mobileNav ? "nav-container-inner active" : "nav-container-inner"}>
+          <ul className="nav">
+            <li>
+              <NavLink exact to="/">Search</NavLink>
+            </li>
+            <li><NavLink to="/history">History</NavLink></li>
+          </ul>
+          <div className="more">
+            {/* <button>More</button> */}
+            { dropDown 
+                ? <FaCaretUp className="caret-icon" onClick={()=>setDropDown(!dropDown)} /> 
+                : <FaCaretDown className="caret-icon" onClick={()=>setDropDown(!dropDown)}/> 
+            }
+            
+            { dropDown && 
+              <div className="dropdown">          
+                <ul>
+                  <li><NavLink to="/page1" onClick={()=>setDropDown(false)}>Page 1</NavLink></li>
+                  <li><NavLink to="/page2" onClick={()=>setDropDown(false)}>Page 2</NavLink></li>
+                </ul>
+              </div>
+            }
+          </div>
+        </div>
+        <div className="mobile-nav-icon" onClick={()=>setMobileNav(!mobileNav)}>
+          { mobileNav ? <FaTimes className='fa-times' /> : <FaBars className='fa-bars' /> }
         </div>
       </div>
     </nav>
